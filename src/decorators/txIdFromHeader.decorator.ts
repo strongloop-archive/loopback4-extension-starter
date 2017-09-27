ce16d6af40b19819d855dae4f7035eec6786a634
Taranveer Virk <taranveer@virk.cc>
2017-09-27 19:32:22 -0400

    feat: add a basic decorator
    Implement a `@txIdFromHeader()` decorator that can annotate a
    controller operation to receive the value of `X-Trasaction-Id` Header.

---
+++ src/decorators/txIdFromHeader.decorator.ts
@@ -0,0 +1,14 @@
+// Copyright IBM Corp. 2017. All Rights Reserved.
+// Node module: loopback-next-extension-starter
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+import {param} from '@loopback/rest';
+
+/**
+ * Decorator to inject the current transaction-id from a request's
+ * 'X-Transaction-Id' header into the decorated function.
+ */
+export function txIdFromHeader() {
+  return param.header.string('X-Transaction-Id');
+}

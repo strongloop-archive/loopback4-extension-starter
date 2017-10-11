29bff47d94380587e8e42f91dbde8eb7b48ae740
Taranveer Virk <taranveer@virk.cc>
2017-10-11 15:17:31 -0400

    feat: add a basic decorator (#6)
    Implement a `@txIdFromHeader()` decorator that can annotate a
    controller operation to receive the value of `X-Trasaction-Id` Header.

---
+++ test/acceptance/decorators/txIdFromHeader.acceptance.ts
@@ -0,0 +1,57 @@
+// Copyright IBM Corp. 2017. All Rights Reserved.
+// Node module: loopback-next-hello-extension
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+import {Application} from '@loopback/core';
+import {get, RestComponent, RestServer} from '@loopback/rest';
+import {txIdFromHeader} from '../../..';
+import {Client, createClientForHandler} from '@loopback/testlab';
+
+describe('@txIdFromHeader() (acceptance)', () => {
+  let app: Application;
+  let server: RestServer;
+
+  beforeEach(createApp);
+  beforeEach(createController);
+  beforeEach(getServerFromApp);
+
+  it('works with header set', async () => {
+    const client: Client = createClientForHandler(server.handleHttp);
+
+    await client
+      .get('/')
+      .set('X-Transaction-Id', 'testid123')
+      .expect(200, 'Your id is testid123');
+  });
+
+  it('works without header', async () => {
+    const client: Client = createClientForHandler(server.handleHttp);
+
+    await client.get('/').expect(200, 'Your id is undefined');
+  });
+
+  async function createApp() {
+    app = new Application({
+      components: [RestComponent],
+      rest: {
+        port: 3000,
+      },
+    });
+  }
+
+  function createController() {
+    class MyController {
+      @get('/')
+      test(@txIdFromHeader() txId: string) {
+        return `Your id is ${txId}`;
+      }
+    }
+
+    app.controller(MyController);
+  }
+
+  async function getServerFromApp() {
+    server = await app.getServer(RestServer);
+  }
+});
---
+++ test/unit/decorators/txIdFromHeader.unit.ts
@@ -0,0 +1,27 @@
+// Copyright IBM Corp. 2017. All Rights Reserved.
+// Node module: loopback-next-hello-extension
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+import {expect} from '@loopback/testlab';
+import {get, getControllerSpec} from '@loopback/rest';
+import {txIdFromHeader} from '../../..';
+
+describe('@txHeaderFromId() (unit)', () => {
+  it('defines a parameter for X-Transaction-Id', () => {
+    class MyController {
+      @get('/')
+      hello(@txIdFromHeader() txId: string) {}
+    }
+
+    const actualSpec = getControllerSpec(MyController);
+
+    expect(actualSpec.paths['/']['get'].parameters).to.eql([
+      {
+        name: 'X-Transaction-Id',
+        type: 'string',
+        in: 'header',
+      },
+    ]);
+  });
+});
